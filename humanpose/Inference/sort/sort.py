d2277947e20f9f375e23c2a4de2b2f796fe41ce8
liranmanzouri <liranman77@gmail.com>
2024-09-18 01:12:27 +0300

    Updated requirements.txt

--- humanpose/Inference/sort/sort.py
+++ humanpose/Inference/sort/sort.py
@@ -30,7 +30,6 @@ def linear_assignment(cost_matrix):
         _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
         return np.array([[y[i], i] for i in x if i >= 0])
     except ImportError:
-        from scipy.optimize import linear_sum_assignment
         x, y = linear_sum_assignment(cost_matrix)
         return np.array(list(zip(x, y)))
 
